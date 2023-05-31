95e1bdf7fef7780d3a340d51f66667faf513dcd9
regayah <105996160+regayahfa@users.noreply.github.com>
2023-05-31 11:07:39 +0300

    Add files via upload

---
+++ project5/src/main/java/com/example/project5/Controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.project5.Controller;
+
+import com.example.project5.Apirespons.Apirespons;
+import com.example.project5.model.User;
+import com.example.project5.sirves.UserServis;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+    private final UserServis userServis;
+
+    @GetMapping("/get")
+    public ResponseEntity getAllUser() {
+        List<User> userList=userServis.getAllUser();
+        return ResponseEntity.status(200).body(userList);
+    }
+    @PostMapping("/add")
+    public ResponseEntity addUser(@Valid @RequestBody User user) {
+        userServis.addUser(user);
+        return ResponseEntity.status(200).body(new Apirespons("user added"));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateUser(@Valid @RequestBody User user, @PathVariable Integer id) {
+        userServis.updateUser(user,id);
+        return ResponseEntity.status(200).body("users Updated");
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteUser(@PathVariable Integer id) {
+        userServis.deleteUser(id);
+        return ResponseEntity.status(200).body("user deleted");
+
+    }
+    @GetMapping("/get/hoppy")
+public  ResponseEntity getUserByHoppy(@Valid @RequestBody User user, @PathVariable String hoppy){
+        userServis.getUserByHoppy(hoppy);
+        return ResponseEntity.status(200).body("added");
+}
+
+}
